d6b5f5c410da63981ea8d5467b09b841daae1a7d
Иван Деркачев <longrail@macbook-pro-evgenij.local>
2019-03-16 22:56:37 +0300

    Initial Commit

---
+++ lesson3-Quiz/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  lesson3-Quiz
+//
+//  Created by Иван longrail on 16/03/2019.
+//  Copyright © 2019 Иван longrail. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
